eef711271317296e5f63d6a5b907c4650ce116fa
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-23 16:14:11 -0800

    commit tree 362c0b82de06d970ff83d1b3ffbf1b5e6776eadd

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    return(
+        <div>
+            <button onBlur= {handleBlur} onFocus={handleFocus}>Eyes on me</button>
+        </div>
+    )
+    function handleBlur(){
+        console.log("Hey! Eyes on me!")
+    }
+    function handleFocus(){
+        console.log("Good!")
+    }
+}
+
+export default EyesOnMe;
